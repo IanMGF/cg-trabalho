691a42c93a44da8f37ebd0ffa79f70ed23983db1
Nmora511 <jomoraesneto@gmail.com>
2025-09-08 16:29:25 -0300

    feat: change planet

--- planet.cpp
+++ planet.cpp
@@ -1,20 +1,28 @@
 #include <GL/freeglut_std.h>
 #include <GL/glut.h>
-#include <vector>
+#include "vec3.cpp"
 
 class Planet {
     private:
         float radius;
         float distance;
         float mass;
-        
+        Vec3 position;
+        Vec3 speed;
+
     public:
         Planet(float radius, float distance, float mass) {
             this->radius = radius;
             this->distance = distance;
             this->mass = mass;
+
+            this->position = new Vec3(0, 0, distance);
         }
-    
+
+        void math_update(){
+
+        }
+
         void draw() {
             glColor3f(0.0f, 1.0f, 0.0f);
             glPushMatrix();
