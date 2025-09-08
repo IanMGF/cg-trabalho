1e6105ccd31e19489581accd2afffe534dfa8fbc
IanMGF <ianmarcosgomesefreitas@gmail.com>
2025-09-08 16:04:33 -0300

    feat: add vec3.cpp

---
+++ vec3.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+class Vec3 {
+    public:
+        float x, y, z;
+        Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+        Vec3 operator+(const Vec3& other) const {
+            return Vec3(x + other.x, y + other.y, z + other.z);
+        }
+
+        Vec3 operator-(const Vec3& other) const {
+            return Vec3(x - other.x, y - other.y, z - other.z);
+        }
+
+        Vec3 operator*(float scale) const {
+            return Vec3(x * scale, y * scale, z * scale);
+        }
+
+        Vec3 operator/(float scale) const {
+            return Vec3(x / scale, y / scale, z / scale);
+        }
+
+        float length() const {
+            return sqrt(x * x + y * y + z * z);
+        }
+
+        Vec3 unitary() const {
+            float length = this->length();
+            return Vec3(x / length, y / length, z / length);
+        }
+};
