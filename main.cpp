bc5e296026600f1d17a9e70418b01ee39fe9bd10
IanMGF <ianmarcosgomesefreitas@gmail.com>
2025-09-10 08:56:09 -0300

    feat: add sun explosion animation

--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
 #include <GL/freeglut_std.h>
 #include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
-#include <iostream>
 #include <cmath>
 #include "body.cpp"
 
 #define G 6.67430e-11
-#define DELTA 1
-#define STEPS 1000
-#define FACTOR 200
+#define DELTA 2
+int steps = 250;
+int factor = 800;
 
-GLfloat angle, fAspect, largura, altura, xcamera, ycamera, zcamera;
+Vec3 camera_pos = Vec3(0, 25000 , 0);
+Vec3 camera_target = Vec3(0, 0, 0);
+Vec3 camera_up = Vec3(0, 0, 1);
+float camera_angle = 0.0;
+
+float sun_step_change_timer = 0;
+int sun_explosion_step = 0;
+float sun_scale = 1;
+float sun_mass = 4e11;
+Vec3 sun_color = Vec3(1, 1, 0.4);
+
+GLfloat fAspect;
 
 Body bodies[10] = {
     // Sun
-    Body(2000, 0, 4E+11, Vec3(1, 1, 0.4)),
+    Body(2000, 0, sun_mass, sun_color),
 
     // Planets
 
@@ -24,14 +35,14 @@ Body bodies[10] = {
     Body(170, Vec3(250, 0, 5000), 1.6E+6, Vec3(0.89, 0.62, 0.29), Vec3(0.07307147186145904, 0, 0)),
 
     // Timber Heart
-    Body(254, 8600, 3E+6, Vec3(0.22, 0.38, 0.27)),
+    Body(254, 8600, 3E+7, Vec3(0.22, 0.38, 0.27)),
     // Attlerock
-    Body(80, 9500, 5E+7, Vec3(0.62, 0.49, 0.42)),
+    Body(80, Vec3(900, 0, 8600), 5E+5, Vec3(0.62, 0.49, 0.42), Vec3(0.0557164, 0, 0.001491565173)),
 
     // Brittle Hollow
-    Body(272, 11700, 3E+6, Vec3(0.31, 0.26, 0.34)),
+    Body(272, 11700, 3E+7, Vec3(0.31, 0.26, 0.34)),
     // Hollow's Lantern
-    Body(97, 12700, 9.1E+5, Vec3(1, 0.75, 0.13)),
+    Body(97, Vec3(1000, 0, 11700), 9.1E+5, Vec3(1, 0.75, 0.13), Vec3(0.04776831, 0, 0.001415022)),
 
     // Giant's Deep
     Body(500, 16460, 2.2E+7, Vec3(0.11, 0.42, 0.27)),
@@ -47,18 +58,55 @@ Body bodies[10] = {
 };
 
 void update(int _) {
-    for (int _step = 0; _step < STEPS; _step++){
+    float effective_delta = DELTA * factor * 0.001;
+    for (int _step = 0; _step < steps; _step++){
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i != j) {
-                    float acc_factor = G * bodies[j].mass / pow((bodies[i].position - bodies[j].position).length(), 2);
-                    Vec3 acc = (bodies[j].position - bodies[i].position).unitary() * acc_factor;
-                    bodies[i].velocity = bodies[i].velocity + acc * DELTA * FACTOR * 0.001;
-                }
+            for (int j = i + 1; j < 10; j++) {
+                float acc_factor = G / pow((bodies[i].position - bodies[j].position).length(), 2);
+                Vec3 acc = (bodies[j].position - bodies[i].position).unitary() * acc_factor;
+
+                Vec3 step = acc * effective_delta;
+
+                bodies[i].velocity = bodies[i].velocity + step * bodies[j].mass;
+                bodies[j].velocity = bodies[j].velocity - step * bodies[i].mass;
             }
 
-            bodies[i].position = bodies[i].position + bodies[i].velocity * DELTA * FACTOR * 0.001;
+            bodies[i].position = bodies[i].position + bodies[i].velocity * effective_delta;
         }
+    }
+
+    if (sun_explosion_step != 0) {
+        float effective_delta = DELTA * 0.001;
+        sun_step_change_timer += effective_delta;
+        
+        switch(sun_explosion_step) {
+            case 1:
+                sun_scale += 1 * effective_delta / 2;
+                sun_color = sun_color + (Vec3(1, 0, 0) - sun_color) * (effective_delta / 2);
+                sun_mass = sun_mass + (1e11 * effective_delta / 2);
+                if (sun_step_change_timer > 2) {
+                    sun_step_change_timer = 0;
+                    sun_explosion_step = 2;
+                }
+                break;
+            case 2:
+                sun_scale -= (2 - 0.1) * effective_delta;
+                sun_color = sun_color + (Vec3(1, 1, 1) - sun_color) * (effective_delta / 1);
+                sun_mass = sun_mass + (2e11 * effective_delta / 1);
+                if (sun_step_change_timer > 1) {
+                    sun_step_change_timer = 0;
+                    sun_explosion_step = 3;
+                }
+                break;
+            case 3:
+                sun_mass = sun_mass - 5e10 * effective_delta;
+                sun_color = sun_color + (Vec3(0.29, 0.78, 0.99) - sun_color) * effective_delta;
+                sun_scale += 2 * effective_delta;
+                break;
+        }
+        
+        bodies[0].mass = sun_mass;
+        bodies[0].color = sun_color;
     }
     
     glutPostRedisplay();
@@ -66,18 +114,39 @@ void update(int _) {
 }
 
 void draw(void) {
-	//glClear(GL_COLOR_BUFFER_BIT);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	//Desenha objetos 3D (wire-frame)
-	for (Body body : bodies) {
+	for (int i=0; i<10; i++) {
+	    Body body = bodies[i];
+		
 	    glColor3f(body.color.x, body.color.y, body.color.z);
 		glPushMatrix();
 			glTranslated(body.position.x, body.position.y, body.position.z);
-			glutWireSphere(body.radius, 20, 20);
+			
+			if (i == 0 && sun_explosion_step != 0) {
+			    // Sun scaling
+			    glScalef(sun_scale, sun_scale, sun_scale);
+			}
+			
+			glutSolidSphere(body.radius, 50, 50);
 		glPopMatrix();
 	}
-	
+
+	Vec3 cylinder_path = bodies[2].position - bodies[1].position;
+	float angle_rad = std::acos(cylinder_path.z / cylinder_path.length());
+	float angle_deg = angle_rad * 180 / M_PI;
+
+	GLUquadric* quad = gluNewQuadric();
+	glColor3f(0.89, 0.62, 0.29);
+	glPushMatrix();
+        glTranslated(bodies[1].position.x, bodies[1].position.y, bodies[1].position.z);
+
+        glRotated(angle_deg, cylinder_path.y, cylinder_path.x, 0);
+
+        gluCylinder(quad, 50000 / cylinder_path.length(), 50000 / cylinder_path.length(), cylinder_path.length(), 20, 20);
+    glPopMatrix();
+
 	glutSwapBuffers();
 }
 
@@ -87,8 +156,6 @@ void setup(void)
 	glClearColor(0.0f, 0.0f, 0.1f, 1.0f);
 
     glEnable(GL_DEPTH_TEST);
-
-	angle = 45;
 }
 
 // Função usada para especificar o volume de visualização
@@ -99,7 +166,7 @@ void view_setup(void)
 	// Inicializa sistema de coordenadas de projeção
 	glLoadIdentity();
 	// Especifica a projeção perspectiva
-	gluPerspective(90, fAspect, 0.01, 1E+9);
+	gluPerspective(90, fAspect, 1, 1E+9);
 
 	// Especifica sistema de coordenadas do modelo
 	glMatrixMode(GL_MODELVIEW);
@@ -107,28 +174,24 @@ void view_setup(void)
 	glLoadIdentity();
 
 	// Especifica posição do observador e do alvo
-	gluLookAt(0, 25000, 0,  // posição da câmera
+	gluLookAt(camera_pos.x, camera_pos.y, camera_pos.z,  // posição da câmera
               0, 0, 0,          // posição do alvo
-              0, 0, 1);         // vetor UP da câmera
+              camera_up.x, camera_up.y, camera_up.z);         // vetor UP da câmera
 }
 
 // Função callback chamada quando o tamanho da janela é alterado
-void window_change_callback(GLint largura, GLint altura)
+void window_change_callback(GLint new_width, GLint new_height)
 {
-	// Para previnir uma divisão por zero
-	if (altura == 0) altura = 1;
+	if (new_height == 0) new_height = 1;
 
-	// Especifica o tamanho da viewport
-	glViewport(0, 0, largura, altura);
-
-	// Calcula a correção de aspecto
-	fAspect = (GLfloat)largura / (GLfloat)altura;
+	glViewport(0, 0, new_width, new_height);
+	fAspect = (GLfloat) new_width / (GLfloat) new_height;
 
 	view_setup();
 }
 
 // Função callback chamada para gerenciar teclas especiais
-void TeclasEspeciais(int key, int x, int y)
+void special_keys_callback(int key, int x, int y)
 {
 	if (key == GLUT_KEY_UP) {
 		// ycamera += 10;
@@ -149,20 +212,41 @@ void TeclasEspeciais(int key, int x, int y)
 	// glutPostRedisplay();
 }
 // Função callback chamada para gerenciar teclado
-void GerenciaTeclado(unsigned char key, int x, int y) {
-	switch (key) {
-		case ' ':
-			ycamera = 50;
-            xcamera = 50;
-            zcamera = 100;
+void keyboard_callback(unsigned char key_code, int x, int y) {
+	switch (key_code) {
+		case 'r':
+			camera_pos.x = 0;
+            camera_pos.y = 25000;
+            camera_pos.z = 0;
+            camera_angle = 0.0;
 			break;
-/*
-			// movimentacao da camera em z
+
+		// Movimentação da câmera
 		case 'w':
+		    camera_pos.x += 1000;
+			break;
+		case 'a':
+		    camera_pos.z += 1000;
+			break;
+		case 's':
+            camera_pos.x -= 1000;
+            break;
+		case 'd':
+		    camera_pos.z -= 1000;
+			break;
 
-        case 's':
+		case 'q':
+		    camera_angle += 0.03;
+			camera_up = Vec3(sin(camera_angle), 0, cos(camera_angle));
+			break;
+		case 'e':
+		    camera_angle -= 0.03;
+			camera_up = Vec3(sin(camera_angle), 0, cos(camera_angle));
+			break;
 
-*/
+		case 'x':
+            sun_explosion_step = 1;
+            break;
 	}
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   //aplica o zBuffer
     view_setup();
@@ -188,22 +272,20 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
 
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);  //GLUT_DOUBLE trabalha com dois buffers: um para renderização e outro para exibição
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);  //GLUT_DOUBLE trabalha com dois buffers: um para renderização e outro para exibição
 
     glutInitWindowPosition(700,100);
-    largura = 600;
-    altura = 500;
-	glutInitWindowSize(largura,altura);
-	fAspect = (GLfloat)largura / (GLfloat)altura;
-	angle = 90;
+	glutInitWindowSize(1280, 720);
+
+	fAspect = (GLfloat) 1280 / (GLfloat) 720;
     glutCreateWindow("Sistema Solar");
 
     glutTimerFunc(DELTA, update, 0);
 	glutDisplayFunc(draw);
 	glutReshapeFunc(window_change_callback); // Função para ajustar o tamanho da tela
     //glutMouseFunc(GerenciaMouse);
-    glutKeyboardFunc(GerenciaTeclado); // Define qual funcao gerencia o comportamento do teclado
-    glutSpecialFunc(TeclasEspeciais); // Define qual funcao gerencia as teclas especiais
+    glutKeyboardFunc(keyboard_callback); // Define qual funcao gerencia o comportamento do teclado
+    glutSpecialFunc(special_keys_callback); // Define qual funcao gerencia as teclas especiais
 
     // Start
     setup();
--- vec3.cpp
+++ vec3.cpp
@@ -4,7 +4,7 @@ class Vec3 {
         float x, y, z;
         Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
         Vec3() = default;
-        
+
         Vec3 operator+(const Vec3& other) const {
             return Vec3(x + other.x, y + other.y, z + other.z);
         }
